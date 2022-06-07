0260a17cf8c44968738e843746dc31f680882e15
Hadi Abdollahzadeh <ereston.hadi@gmail.com>
2022-06-07 13:09:16 +0430

    Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+       <a href="/">shopy</a>
+      </header>
+      <main>list product</main>
+    </div>
+  );
+}
+
+export default App;
